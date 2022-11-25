15f6bc5cf7931a80595c91d8a6ff2443eb876803
NeightN <neightn7@gmail.com>
2022-11-25 14:36:22 +0100

    added changes

--- headers/header_logged.php
+++ headers/header_logged.php
@@ -1,32 +1,29 @@
 <!-- Header -->
 <header>
-    <!-- Sign up -->
-    <div class="offcanvas offcanvas-end bg-dark text-white" id="profil">
-        <div class="offcanvas-header">
-            <h1 class="offcanvas-title">Profil</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-        </div>
-        <hr class="text-white" style="margin:0">
-        <div class="offcanvas-body">
-
-        </div>
-    </div>
-
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
             <!-- Logo -->
             <a class="navbar-brand" href="#">Homura</a>
+            <div class="d-flex align-items-center">
 
-            <!-- Buttons for "sign up" and "log in" -->
-            <div>
-                <button class="btn btn-primary pfp_btn" id="pfp_btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#profil" title="pfp">
-                    <img height="24" width="24" src="img/coffee_def.svg" alt="coffe_pfp">
-                </button>
-
-                <a class="btn btn-secondary" href="user/logout.php" title="logout">
-                    Log out
-                </a>
+                <div class="dropdown">
+                    <a class="dropdown-toggl" href="#" id="navbarDropdownMenuAvatar" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="img/coffee_def.svg" class="rounded-circle" height="50" alt="avatar" loading="lazy" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item" href="#">My profile</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Settings</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="user/logout.php">Logout</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+        </div>
         </div>
     </nav>
     <!-- Header background -->
