c3eba7f5fea42dc2579fc41bfbb5aeb7456efd51
Prokop Svačina <svacina@SPSEJECNANET.CZ>
2023-01-13 13:44:48 +0100

    Fixed Email auto-sennd query

--- auto/WeeklyMail.php
+++ auto/WeeklyMail.php
@@ -11,60 +11,76 @@ $stmt = $conn->prepare($query);
 $stmt->execute();
 $resultUsr = $stmt->get_result();
 
-echo "<h2>cs</h2>"
+$conn->close();
 
 // Loop through each user and send an email
 while ($user = $resultUsr->fetch_assoc()) {
 
-    
+    echo "<h2>".$user['name']."</h2>";
 
     /* Get drinkn for user */
     $sql = "select types.typ, count(drinks.ID) as pocet, types.davky
     from drinks inner join people on drinks.id_people = people.ID 
     left join types on drinks.id_types = types.ID
     where people.ID = '" . $user['ID'] . "'
-    group by people.name, types.typ;";
+    group by types.typ, types.davky;";
 
+    
+    include("../inc/connection.php");
+
+    
     $result = mysqli_query($conn, $sql);
-    echo "<table class='accordion-body m-0 table table-hover'>
+
+    if (!$result) {
+        echo mysqli_error($conn);
+    }
+
+    $email_content = "";
+
+    $email_content.= "<table class='accordion-body m-0 table table-hover'>
             <tr>
-                <th>Typ pitíčka</th>
-                <th>Počet pitíček</th>
-                <th>Dávka</th>
-                <th>Celkem k úhradě</th>
+                <th>Typ</th>
+                <th>Počet</th>
+                <th>Objem (g)</th>
+                <th>K úhradě</th>
             </tr>";
 
     $array_celkem_cena = array();
     $array_celkem_pocet = array();
     while ($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['typ'] . "</td>";
-        echo "<td>" . $row['pocet'] . "</td>";
-        echo "<td>" . $row['davky'] . "</td>";
+        $email_content.= "<tr>";
+        $email_content.= "<td>" . $row['typ'] . "</td>";
+        $email_content.= "<td>" . $row['pocet'] . "</td>";
+        $email_content.= "<td>" . $row['davky'] . "</td>";
         $davka = $row["davky"];
         $pocet = $row["pocet"];
         $celkem = $davka * $pocet * 300;
         $array_celkem_pocet[] = $pocet;
         $array_celkem_cena[] = $celkem;
-        echo "<td>" . $celkem . " Kč</td>";
+        $email_content.= "<td>" . $celkem . " Kč</td>";
     }
-    echo "<tr><td>Vše</td><td>". array_sum($array_celkem_pocet) ."</td><td></td><td>". array_sum($array_celkem_cena) ." Kč</td></tr>";
-    echo "</table>";
 
 
+    $email_content.= "<tr><td>Vše</td><td>". array_sum($array_celkem_pocet) ."</td><td></td><td>". array_sum($array_celkem_cena) ." Kč</td></tr>";
+    $email_content.= "</table>";
 
+    echo 'Content:';
+    echo $email_content;
+    
 
     $to = $user['email'];
     $subject = 'Piticker - Týdenní Přehled';
-    $message = '
-    
-    ';
+    $message = $email_content;
+
     $headers = 'From: piticker@scp-isolation.com' . "\r\n" .
                'Reply-To: piticker@scp-isolation.com' . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
-    mail($to, $subject, $message, $headers);
+    
+    
+               mail($to, $subject, $message, $headers);
 }
 
 // Close the database connection
-$conn->close();
+
+
 ?>
